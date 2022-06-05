6fa393b8d7b8170eb6d27841fe8cbcab46bcea41
devansh-jain-2002 <d.jain.182002@gmail.com>
2022-06-05 23:01:16 +0530

    Added something

--- auto.py
+++ auto.py
@@ -12,7 +12,8 @@ course_dict={
 "chm102":"https://hello.iitk.ac.in/index.php/course/chm102aa2122",
 "mth102":"https://hello.iitk.ac.in/course/mth102aa2122",
 "phy102":"https://hello.iitk.ac.in/course/phy102aa2122",
-"phy101":"https://hello.iitk.ac.in/course/phy101aa2122"}
+"phy101":"https://hello.iitk.ac.in/course/phy101aa2122",
+"ta101":"https://hello.iitk.ac.in/ta101aa2122"}
 driver.implicitly_wait(5)
 username= driver.find_element_by_id("edit-name")
 username.send_keys("jdevansh21@iitk.ac.in")
